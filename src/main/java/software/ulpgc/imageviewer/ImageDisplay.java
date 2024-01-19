acbf25d3e7460971e91330a05e275498fc60c492
Daniel Cruz López <danielcruzlopez1501@gmail.com>
2024-01-19 15:03:46 +0000

    redu ImageDisplay

--- src/main/java/software/ulpgc/imageviewer/ImageDisplay.java
+++ src/main/java/software/ulpgc/imageviewer/ImageDisplay.java
@@ -1,19 +1,8 @@
 package software.ulpgc.imageviewer;
 
+import software.ulpgc.imageviewer.model.Image;
+
 public interface ImageDisplay {
-    void paint(String id, int offset);
-    int getWidth();
-    void clear();
-    void on(Shift shift);
-    void on(Released released);
-
-    interface Shift {
-        Shift Null = offset -> {};
-        void offset(int offset);
-    }
-
-    interface Released {
-        Released Null = offset -> {};
-        void offset(int offset);
-    }
+    void show(Image image);
+    Image image();
 }
